cbf68e752bcc6dadd101a814dfb1df172887c772
leeneo <leeneo@xingzhihen.com>
2018-01-31 18:11:27 +0800

    1

---
+++ js/dateForward.js
@@ -0,0 +1,11 @@
+﻿function dayForward(startDate, num) {
+
+	var dayTime = 1 * 24 * 60 * 60 * 1000; //一天的毫秒数
+	//console.log("一天的毫秒数：" + dayTime);
+	// var oneday = new Date(dayTime);
+	// console.log(oneday.toLocaleDateString());    //为什么是1970/1/2
+	var fd = new Date(startDate);
+	var resDay = new Date(fd.getTime() + dayTime * num).toLocaleDateString();
+	//console.log(resDay);
+	return resDay;
+}
